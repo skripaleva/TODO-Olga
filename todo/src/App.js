9108145609baf9dc5ae753310f84ac6092272e33
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-04-28 20:37:29 +0300

    elements

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,58 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './number';
 
-const counts = 25;
-const develop = true;
-const user = false;
+const element = (<div> 
+	Hello World!
+	<a href=''>go to</a>
+</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {{
-           fontSize: '45px'
-        }}>
-          Hello!
-        </p>
-        <p> 
-        	Общая длина: { count * length }
-        </p>
-        <p> 
-          {'33'}
-        </p>
-        <p> 
-          {counts}
-        </p>
-        <p> 
-          {2 + 3}
-        </p>
-        <p> 
-          {develop && 'develop is true'}
-          {undefined}
-          {null}
-          {false}
-          {true}
-        </p>
-        <p>
-          {user ? 'user is true' : 'user is false'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
