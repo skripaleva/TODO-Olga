fcfd59954754bae7634b50b7447b3f5e8400bffe
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-04 12:43:05 +0300

    keys

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,22 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Покормить кота'
+const todoItem = 'Покормить кота';
 
-const App = () => (
+const App = () => {
+	const items = [
+		{value: 'Покормить кота'},
+		{value: 'Сходить на работу'},
+		{value: 'Полить цветы'}
+	];
+
+return (
 	<div>
 		<h1>Список дел на сегодня:</h1>
 		<InputItem />
-		<ItemList todoItem={todoItem} />
+		<ItemList items={items} />
 		<Footer count= {3} />
 	</div>);
+}
 
 export default App; 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = ( {todoItem} ) => (<ul>
-	<li><Item todoItem={todoItem} /></li>
-	<li><Item todoItem={'Сходить на работу'} /></li>
-	<li><Item todoItem={'Полить цветы'} /></li>
+const ItemList = ( {items} ) => (<ul>
+	{items.map(item => <li key={item.value}>
+		<Item value={item.value} /></li>)}
 </ul>);
 
 export default ItemList;
