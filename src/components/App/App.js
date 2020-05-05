9b1aa0f2b028bf41702638d9ba172eb15e5837e9
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-05 17:49:13 +0300

    style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const todoItem = 'Покормить кота';
 
@@ -13,8 +14,8 @@ const App = () => {
 	];
 
 return (
-	<div>
-		<h1>Список дел на сегодня:</h1>
+	<div className='wrap'>
+		<h1 className='wrap__title'>Список дел на сегодня:</h1>
 		<InputItem />
 		<ItemList items={items} />
 		<Footer count= {3} />
