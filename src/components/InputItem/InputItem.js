a228983d69c80a5596432be6f21d65345f4d4f0a
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-06 18:26:05 +0300

    ui

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>InputItem</div>);
+const InputItem = () => (<div>
+	<TextField 
+		id="outlined-basic" 
+		label="Добавить новое дело" 
+		variant="outlined"
+	/>
+</div>);
 
 export default InputItem;
