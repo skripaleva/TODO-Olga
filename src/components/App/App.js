e46422d7dd05622424180d05d07fffddeb6830a9
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-24 18:50:37 +0300

    25

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,20 +14,20 @@ import styles from './App.module.css';
 
 const App = () => (<Router>
   <div className={styles.wrap}>
-      <MenuList className={styles.sidebar}>
-          <Link to='/' className={styles.link}>
-              <MenuItem>Обо мне</MenuItem></Link>
+    <MenuList className={styles.sidebar}>
+      <Link to='/' className={styles.link}>
+        <MenuItem>Обо мне</MenuItem></Link>
           <Link to='/todo' className={styles.link}>
-              <MenuItem>Дела</MenuItem></Link>
+            <MenuItem>Дела</MenuItem></Link>
           <Link to='/contacts' className={styles.link}>
-              <MenuItem>Контакты</MenuItem></Link>
-      </MenuList>
+            <MenuItem>Контакты</MenuItem></Link>
+    </MenuList>
     <div>
       <Route path='/' exact component={About} />
       <Route path='/todo' component={Todo} />
       <Route path='/contacts' component={Contacts} />
     </div>
-    </div>
+  </div>
 </Router>);
 
 
