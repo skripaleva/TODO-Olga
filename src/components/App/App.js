7dc7edd59440a44cd552bb0efabc39c8d39b69ba
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-24 18:45:04 +0300

    24

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,21 +13,22 @@ import MenuList from '@material-ui/core/MenuList';
 import styles from './App.module.css';
 
 const App = () => (<Router>
-    <div className={styles.wrap}>
-        <MenuList className={styles.sidebar}>
-            <Link to='/' className={styles.link}>
-                <MenuItem>Обо мне</MenuItem></Link>
-            <Link to='/todo' className={styles.link}>
-                <MenuItem>Дела</MenuItem></Link>
-            <Link to='/contacts' className={styles.link}>
-                <MenuItem>Контакты</MenuItem></Link>
-        </MenuList>
-            <div>
-                <Route path='/' exact component={About} />
-                <Route path='/todo' component={Todo} />
-                <Route path='/contacts' component={Contacts} />
-            </div>
+  <div className={styles.wrap}>
+      <MenuList className={styles.sidebar}>
+          <Link to='/' className={styles.link}>
+              <MenuItem>Обо мне</MenuItem></Link>
+          <Link to='/todo' className={styles.link}>
+              <MenuItem>Дела</MenuItem></Link>
+          <Link to='/contacts' className={styles.link}>
+              <MenuItem>Контакты</MenuItem></Link>
+      </MenuList>
+    <div>
+      <Route path='/' exact component={About} />
+      <Route path='/todo' component={Todo} />
+      <Route path='/contacts' component={Contacts} />
+    </div>
     </div>
 </Router>);
+
 
 export default App; 
