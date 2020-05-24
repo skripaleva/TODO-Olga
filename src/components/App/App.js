97a2959a19ef466d348446a2e1872c906dc643ce
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-05-24 15:01:06 +0300

    15

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-	BrowserRouter as Router,
-	Route,
-	Link,
+    BrowserRouter as Router,
+    Route,
+    Link,
 } from 'react-router-dom';
 import Todo from '../Todo/Todo';
 import About from '../About/About';
