ce2450bc33306fdbb33fd9ec34c6472fbbdfb8b6
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2020-06-01 19:31:39 +0300

    1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Octokit } from '@octokit/rest';
+import { 
+  Octokit, 
+} from '@octokit/rest';
 import styles from './About.module.css';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const octokit = new Octokit();
 
 class About extends React.Component {
+
   state = {
     isLoading: true,
     repoList: [],
         isError: false,
         userInfo: [],
-        username: 'MylnikovaO'
+        username: 'MylnikovaO',
   };
 
   componentDidMount() {
 
     octokit.repos.listForUser({
-      username: this.state.username
-    }).then(({ data }) => {
-      this.setState({
-                repoList: data,
-                isLoading: false
-            });
+      username: this.state.username,
     })
+
+    .then(({ data }) => {
+      this.setState({
+        repoList: data,
+        isLoading: false,
+      });
+    });
+
       .catch(() => {
         this.setState({
           isError: true,
@@ -39,6 +45,7 @@ class About extends React.Component {
             name: response.data.name
         });
     })
+
           .catch(err => {
             this.setState({
               isLoading: false,
@@ -48,10 +55,17 @@ class About extends React.Component {
     }
 
     render () {
-      const { isLoading, repoList, isError, name, avatarURL} = this.state;
+      const { 
+        isLoading, 
+        repoList, 
+        isError, 
+        name, 
+        avatarURL,
+      } = this.state;
         return (
           <div className={styles.wrap}>
-            {isLoading ? <div className={styles.loader}> <CircularProgress /> </div> :
+            {isLoading ? <div className={styles.loader}> 
+            <CircularProgress /> </div> :
               <div className={styles.info}>
                 <h1 className={styles.name}>{name}</h1>
                 {isError ? <div className={styles.error}>Возникла ошибка</div> :
@@ -60,7 +74,7 @@ class About extends React.Component {
                     <img src={avatarURL} className={styles.repo_avatar} />
                   </div>
                   <div>
-                    <p className={styles.repo_text}> Мои репозитории:</p> 
+                    <p className={styles.repo_text}> Мои репозитории:</p>
                     <ol className={styles.repo_list}>
                       {repoList.map(repo => (<li key={repo.id}>
                         <a href={repo.id} className={styles.repo_name}>{repo.name}</a>
@@ -73,8 +87,9 @@ class About extends React.Component {
               </div>
             }
           </div>
-        )
+        );
     }
+
 };
 
 export default About;
